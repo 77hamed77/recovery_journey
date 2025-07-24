fbba2995a04ae8a49e5fa8648f74eb0377a8767f
Hamid <h55180d@gmail.com>
2025-07-24 14:17:50 +0300

    تعديلات

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # تطبيق المستخدمين (تسجيل/دخول/مستخدمين)
+    path('users/', include('users.urls')),
     path(
     'users/',
     include(
